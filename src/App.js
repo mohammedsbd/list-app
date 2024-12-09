d8722ca57c1f87669e1522b31810036b78a04642
mohammedsbd <Mohammed1112ok@gmail.com>
2024-12-09 01:15:50 -0800

    add an array of objects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import React from "react";
 
+
+const cards = [
+  {
+    name: "Mohammed Salih",
+    bio: "He is an otaku aka Anime Addict",
+  },
+  {
+    name: "Bill Gates",
+    bio: "He is the richest man in the world, at least he was",
+  },
+  {
+    name: "Roronoa Zoro",
+    bio: "He wields three blades",
+  },
+  {
+    name: "Monkey D. Luffy",
+    bio: "He is gonna be the king of the pirates",
+  },
+  {
+    name: "Sosuke Aizen",
+    bio: "He is a manipulative man",
+  },
+];
+
 function App() {
   return (
     <div className="container">
@@ -8,7 +32,8 @@ function App() {
 
 
       <div className="cards">
-        <article>
+        
+        {/* <article>
           <h2>Mohammed Salih</h2>
           <p>lorem</p>
         </article>
@@ -31,7 +56,7 @@ function App() {
         <article>
           <h2>Sosuke Aizen</h2>
           <p>lorem</p>
-        </article>
+        </article> */}
       </div>
     </div>
   );

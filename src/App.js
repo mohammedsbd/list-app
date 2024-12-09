b7707a104a59be5e4ca1f63903be2ebe4205f77c
mohammedsbd <Mohammed1112ok@gmail.com>
2024-12-09 01:00:57 -0800

    added styling to the lists

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>List App</h1>
+
+
+
+      <div className="cards">
+        <article>
+          <h2>Mohammed Salih</h2>
+          <p>lorem</p>
+        </article>
+
+        <article>
+          <h2>Bill Gates</h2>
+          <p>lorem</p>
+        </article>
+
+        <article>
+          <h2>Roronoa Zoro</h2>
+          <p>lorem</p>
+        </article>
+
+        <article>
+          <h2>Monkey D. Lufyy </h2>
+          <p>lorem</p>
+        </article>
+
+        <article>
+          <h2>Sosuke Aizen</h2>
+          <p>lorem</p>
+        </article>
+      </div>
+    </div>
+  );
+}
+
+export default App;

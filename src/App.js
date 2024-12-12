d722a3dee366dfed085bdd02ebff66aa45ba9ea5
mohammedsbd <Mohammed1112ok@gmail.com>
2024-12-12 07:31:43 -0800

    finalized

--- src/App.js
+++ src/App.js
@@ -6,19 +6,26 @@ import  { cards } from "./data"
 function App() {
 
 const [people,setPeople]=useState(cards)
+const handelDelete = (id) => { 
+  setPeople(people.filter((person) => person.id !== id))
+
+
+
+
+}
 
   return (
     <div className="container">
-      <h1>List App</h1>
+      <h1>List App {people.length}</h1>
 
       <div className="cards">
         {console.log("this is working")}
-        {people.map(({name, bio}) =>(
+        {people.map(({name, bio, id}) =>(
     <article key={bio}>
 
               <h2>{name}</h2>
               <p>{bio}</p>
-              <button className="danger">Remove</button>
+              <button className="danger" onClick={()=>handelDelete(id)}>Remove</button>
             </article>
         ))}
           
--- src/data.js
+++ src/data.js
@@ -1,21 +1,28 @@
  export const cards = [
   {
+    id:1,
     name: "Mohammed Salih",
     bio: "He is an otaku aka Anime Addict",
   },
   {
+    id:2,
     name: "Bill Gates",
     bio: "He is the richest man in the world, at least he was",
   },
   {
+    id:3,
     name: "Roronoa Zoro",
     bio: "He wields three blades",
   },
   {
+    id:4,
     name: "Monkey D. Luffy",
     bio: "He is gonna be the king of the pirates",
   },
   {
+    id:5,
+    name: "Saitama",
+    bio: "He is a super saiyan",
     name: "Sosuke Aizen",
     bio: "He is a manipulative man",
   },

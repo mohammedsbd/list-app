a9c6ae9bcc9c50e09a787c5fbc1855d1a47b1502
mohammedsbd <Mohammed1112ok@gmail.com>
2024-12-12 06:39:46 -0800

    added a separate data file

--- src/App.js
+++ src/App.js
@@ -1,47 +1,24 @@
 import React from "react";
+import  { cards } from "./data"
 
 
-const cards = [
-  {
-    name: "Mohammed Salih",
-    bio: "He is an otaku aka Anime Addict",
-  },
-  {
-    name: "Bill Gates",
-    bio: "He is the richest man in the world, at least he was",
-  },
-  {
-    name: "Roronoa Zoro",
-    bio: "He wields three blades",
-  },
-  {
-    name: "Monkey D. Luffy",
-    bio: "He is gonna be the king of the pirates",
-  },
-  {
-    name: "Sosuke Aizen",
-    bio: "He is a manipulative man",
-  },
-];
 
 function App() {
   return (
     <div className="container">
       <h1>List App</h1>
 
-
-
       <div className="cards">
         {console.log("this is working")}
-        {cards.map((singelCard, index) =>{
-          return(
-            <article key={index}>
+        {cards.map(({name, bio}) =>(
+    <article key={bio}>
 
-              <h2>{singelCard.name}</h2>
-              <p>{singelCard.bio}</p>
+              <h2>{name}</h2>
+              <p>{bio}</p>
             </article>
-          )
-        })}
+        ))}
+          
+        
 
 
 
---
+++ src/data.js
@@ -0,0 +1,22 @@
+ export const cards = [
+  {
+    name: "Mohammed Salih",
+    bio: "He is an otaku aka Anime Addict",
+  },
+  {
+    name: "Bill Gates",
+    bio: "He is the richest man in the world, at least he was",
+  },
+  {
+    name: "Roronoa Zoro",
+    bio: "He wields three blades",
+  },
+  {
+    name: "Monkey D. Luffy",
+    bio: "He is gonna be the king of the pirates",
+  },
+  {
+    name: "Sosuke Aizen",
+    bio: "He is a manipulative man",
+  },
+];
